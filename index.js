e5e5a3e33125d447aa729ba399653f07f067ed56
Daniel Murphy <danielmurphy02@gmail.com>
2018-12-14 17:05:09 -0800

    WIP

--- index.js
+++ index.js
@@ -1,45 +1,111 @@
 const path = require('path');
-const { RTMClient, WebClient } = require('@slack/client');
+const { RTMClient } = require('@slack/client');
+const sharp = require('sharp');
+const request = require('request').defaults({ encoding: null });
+const fs = require('fs');
 
 class ControlDeckSlack {
-	constructor(streamDeck, buttonId, options) {
-		this.streamDeck = streamDeck;
-		this.buttonId = buttonId;
-		let myPresence = 'away';
+  constructor(streamDeck, buttonId, options) {
+    console.log('still here');
+    if (options.type === 'toggle-status') {
+      new ToggleStatus(streamDeck, buttonId, options);
+    } else if (options.type === 'show-user-status') {
+      new ShowUserStatus(streamDeck, buttonId, options);
+    }
+  }
+}
 
-		const slackToken = process.env.SLACK_API_TOKEN;
-		const web = new WebClient(slackToken);
-		const rtm = new RTMClient(slackToken);
+class ShowUserStatus {
+  constructor(streamDeck, buttonId, options) {
+    this.streamDeck = streamDeck;
+    this.buttonId = buttonId;
+    this.userId = options.user_id;
 
-		this._updateButton(myPresence);
+    const slackToken = process.env.SLACK_API_TOKEN;
+    // const web = new WebClient(slackToken);
+    const rtm = new RTMClient(slackToken);
 
-		rtm.start({
-			batch_presence_aware: true
-		});
+    // web.users.info({ user: this.userId }).then(data => {
 
-		rtm.subscribePresence([options.user_id]);
+    // console.log('heyo');
+    // const userAvatarURL = data.user.profile.image_72;
+    const userAvatarURL =
+      'https://avatars.slack-edge.com/2018-04-10/344384085874_2eec81c1f90277cd0bb6_72.jpg';
+    request(userAvatarURL, (error, response, body) => {
+      // sharp(body)
+      //   .flatten()
+      //   .resize(streamDeck.ICON_SIZE, streamDeck.ICON_SIZE)
+      //   .raw()
+      //   .toBuffer()
+      //   .then(buffer => {
+      //     this.onlineImage = buffer;
+      //   });
+      sharp(body)
+        .flatten()
+        .resize(streamDeck.ICON_SIZE, streamDeck.ICON_SIZE)
+        .raw()
+        .toBuffer()
+        .then(buffer => {
+          this.offlineImage = buffer;
+        });
+    });
+    // });
 
-		rtm.on('presence_change', event => {
-			myPresence = event.presence;
-			this._updateButton(myPresence);
-		});
+    rtm.start({
+      batch_presence_aware: true
+    });
 
-		streamDeck.on('up', keyIndex => {
-			if (keyIndex == buttonId) {
-				let newStatus = myPresence === 'away' ? 'auto' : 'away';
-				web.users.setPresence({ presence: newStatus });
-				this._updateButton(newStatus);
-			}
-		});
-	}
+    rtm.subscribePresence([this.userId]);
 
-	_updateButton(status) {
-		let icon = status === 'away' ? 'offline.png' : 'online.png';
-		this.streamDeck.fillImageFromFile(
-			this.buttonId,
-			path.resolve(__dirname, `icons/${icon}`)
-		);
-	}
+    rtm.on('presence_change', event => {
+      console.log(`setting to ${event.presence}`);
+      event.presence === 'active'
+        ? streamDeck.fillImage(this.buttonId, this.onlineImage)
+        : streamDeck.fillColor(this.buttonId, this.offlineImage);
+    });
+  }
+}
+
+class ToggleStatus {
+  constructor(streamDeck, buttonId, options) {
+    this.streamDeck = streamDeck;
+    this.buttonId = buttonId;
+    let myPresence = 'away';
+
+    const slackToken = process.env.SLACK_API_TOKEN;
+    // const web = new WebClient(slackToken);
+    const rtm = new RTMClient(slackToken);
+
+    this._updateButton(myPresence);
+
+    rtm.start({
+      batch_presence_aware: true
+    });
+
+    rtm.subscribePresence([options.user_id]);
+
+    rtm.on('presence_change', event => {
+      myPresence = event.presence;
+      this._updateButton(myPresence);
+    });
+
+    streamDeck.on('up', keyIndex => {
+      console.log('up!');
+      if (keyIndex === buttonId) {
+        let newStatus = myPresence === 'away' ? 'auto' : 'away';
+        // web.users.setPresence({ presence: newStatus });
+        this._updateButton(newStatus);
+      }
+    });
+  }
+
+  _updateButton(status) {
+    let icon = status === 'away' ? 'offline.png' : 'online.png';
+    this.streamDeck.fillImageFromFile(
+      this.buttonId,
+      path.resolve(__dirname, `icons/${icon}`)
+    );
+  }
 }
 
 module.exports = ControlDeckSlack;
